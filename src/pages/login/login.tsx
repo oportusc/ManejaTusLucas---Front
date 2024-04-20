03b9a9db2f9e084eb3a15fa0e2378b640ab895d0
oportusc <oportus.c@gmail.com>
2024-04-20 14:59:06 -0400

    first commit

---
+++ src/pages/login/login.tsx
@@ -0,0 +1,8 @@
+export const Login =()=>{
+    return (
+        <>
+        <h1 className='text-center text-3xl mt-20'>Maneja Tus Lucas!!!</h1>
+        
+        </>
+    )
+}
---
+++ src/router/router.tsx
@@ -0,0 +1,15 @@
+import { createBrowserRouter } from 'react-router-dom';
+import { Login } from '../pages/login/login';
+
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    children: [
+        {
+            path: 'login',
+            element: <Login />
+          },
+    ],
+  }
+])
